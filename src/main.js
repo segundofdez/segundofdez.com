bc1826e76c287008aa2c49c487abc29c157124ac
Segundo Fdez <segun.2@gmail.com>
2024-12-30 11:59:58 +0100

    delete intrusive js. fix #1

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { themetoggle } from "./js/theme-toggle.js";
 // import { setupButtonEvents } from "./js/setup-buttons.js";
 // import { randomizeImages } from './js/img-random.js';
 
-gsap.registerPlugin(Draggable);
+// gsap.registerPlugin(Draggable);
 
 document.addEventListener("DOMContentLoaded", function () {
     customCursor();
